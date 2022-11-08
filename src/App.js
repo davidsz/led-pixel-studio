249012c31b73925cbd685ac5c52a6b2cae214e8d
Szabolcs David <davidsz@inf.u-szeged.hu>
2022-11-08 01:36:44 +0100

    Start new project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>LED Pixel Studio</h1>
+        </div>
+    );
 }
 
 export default App;
